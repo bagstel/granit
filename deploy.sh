aac0876cbc64d5ac7414023693ea862163b8df61
bagstel <stelios@baglaridis.com>
2019-11-10 05:43:35 +0200

    webpack template init

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+## abort on errors
+set -e
+
+## build
+npm run build
+
+## navigate into the build output directory
+cd dist
+
+## if you are deploying to a custom domain
+# echo 'simple.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+## if you are deploying to https://<USERNAME>.github.io
+# git add remote origin https://github.com/<USERNAME>/<REPO>.git
+# git push -f origin master
+
+## if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git remote add origin https://<USERNAME>.github.io/<REPO>
+# git push -f origin master:gh-pages
+
+cd -
+
+## https://cli.vuejs.org/guide/deployment.html#github-pages
